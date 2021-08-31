30d72447467d48b19399c787a0407dc7ad41b079
WillHDev <will.v.holcombe@gmail.com>
2021-08-31 00:04:58 -0600

    A and B nesting

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,35 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+
+function CompA() {
   return (
-    <h1>Hello world</h1>
+    <>
+    <h1>CompA</h1>
+    <p>Hello Component A</p>
+    <CompB />
+    </>
   )
 }
+
+
+function CompB() {
+  return (
+    <>
+    <h1>CompB</h1>
+    <p>Hello CompB</p>
+    </>
+  )
+}
+
+ function Home() {
+  return (
+    <>
+    <h1>Hello world</h1>
+    <CompA />
+    </>
+  )
+}
+
+
+export default Home
