41187bd0d6c938b81c505138071404dcb297af49
WillHDev <will.v.holcombe@gmail.com>
2021-09-02 11:14:28 -0600

    added doc template

--- pages/index.js
+++ pages/index.js
@@ -102,6 +102,55 @@ const Home = () => {
   //or state changed
 
   //list of dependencies go in array
+
+  useEffect(() => {
+    // const script = document.createElement('script');
+  
+    // script.src = "https://use.typekit.net/foobar.js";
+    // script.async = true;
+  
+    // document.body.appendChild(script);
+  
+    // return () => {
+    //   document.body.removeChild(script);
+
+
+    // }
+
+    <script>
+      { 
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+})
+}
+  </script>
+  }, []);
+
+
+
+
   useEffect( () => {
     console.log("useEffect called");
   }, [myValue])
